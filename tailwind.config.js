0bf56caf51027b00b27de0c62639617e1db57257
kamba tukebele <kambatukebele@gmail.com>
2024-08-05 19:17:00 +0200

    created scrolling-content section

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,15 @@ export default {
           "scrollbar-width": "none",
         },
       },
+      keyframes: {
+        scroll: {
+          '0%' : { transform: 'translateX(0)'},
+          '100%' : { transform: 'translateX(-100%)'}
+        }
+      },
+      animation: {
+        scroll: 'scroll 20s linear infinite'
+      }
     },
   },
   plugins: [
